dbc1fb091a12bf50bf52b74b86528e5c541941d4
maxsteffen <95710214+maxsteffen@users.noreply.github.com>
2024-02-27 11:17:08 -0600

    Update dispatch.c

--- dispatch.c
+++ dispatch.c
@@ -21,27 +21,26 @@
 
 void dispatch(ulong cause, ulong val, ulong *frame, ulong *program_counter) {
     ulong swi_opcode;
-    
+
     if((long)cause > 0) {
         cause = cause << 1;
         cause = cause >> 1;
 
         /**
-		* TODO:
-		* 
-		* Check to ensure the trap is an environment call from U-Mode
-		*/
-		if (cause != E_ENVCALL_FROM_UMODE){ 	
-		//* If the trap is not an environment call from U-Mode call xtrap
-			xtrap(*frame, cause, val, *program_counter);
-		
-		}
-		//* Find the system call number that's triggered
-		ulong syscall_num = CTX_A7(frame); // this is where I think the syscall number is but I'm not sure will ask brylow tomorrow.
-		//* Pass the system call number and any arguments into syscall_dispatch. Make sure to set the return value in the appropriate spot.
-		CTX_RA(frame) = syscall_dsipatch(syscall_num, *frame); // Not sure where exactly on the frame the arguments for syscall are stored.
-		//* Update the program counter appropriately with set_sepc
-		
+                * TODO:
+                * 
+                * Check to ensure the trap is an environment call from U-Mode
+                */
+                if (cause != E_ENVCALL_FROM_UMODE){
+                //* If the trap is not an environment call from U-Mode call xtrap
+                        xtrap(frame, cause, val, program_counter);
+
+                }
+                //* Find the system call number that's triggered
+                ulong syscall_num = frame[CTX_A7]; // this is where I think the syscall number is but I'm not sure will ask brylow tomorrow.
+                //* Pass the system call number and any arguments into syscall_dispatch. Make sure to set the return value in the appropriate spot.
+                frame[CTX_A0] = syscall_dispatch(syscall_num, &frame[CTX_A0]); // Not sure where exactly on the frame the arguments for syscall are stored.
+                //* Update the program counter appropriately with set_sepc
+                set_sepc(program_counter + 4);
     }
 }
-	
